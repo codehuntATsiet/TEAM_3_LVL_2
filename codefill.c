68818ec517d23fbf3d633d191d73e993dc0bcc33
codehuntATsiet <127717140+codehuntATsiet@users.noreply.github.com>
2023-03-15 15:47:07 +0530

    Add files via upload

---
+++ codefill.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+void printClue(int );
+int findSum(int );
+
+int main()
+{
+    // write a code to find the sum of square of each digit  
+    // using atleast 1 function
+    // let the given number is n=345
+    // then we need 3*3+4*4+5*5
+    // constraint n>99 & n<100000
+    int n=0;
+    // write a code get inputs for n
+
+
+
+
+    int sum=findSum(n);
+
+
+    printClue(sum);
+    return 0;
+}
+int findSum()
+{
+    int sum=0;
+    // write a code here to find the sum
+
+
+
+
+
+    return sum;
+}
+
+
+void printClue(int x)
+{
+    printf("%c",x);
+
+    
+
+}
